3f34e58e80a244d9e0bb7c1baa001e8e68539e36
daejoon <kkode1911@gmail.com>
2024-12-31 00:10:54 +0900

    Remove unnecessary @Testcontainers annotation

--- src/test/java/com/ddoong2/requiresnew/repository/UserRepositoryTest.java
+++ src/test/java/com/ddoong2/requiresnew/repository/UserRepositoryTest.java
@@ -8,12 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
 @ActiveProfiles("test")
-@Testcontainers
 @DataJpaTest
 class UserRepositoryTest {
 
