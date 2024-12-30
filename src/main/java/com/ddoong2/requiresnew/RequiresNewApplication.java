167c7f8b881c2e128159e572f2fa8d064c2d656e
고대준 <daejoon.ko@hanatour.com>
2024-12-30 18:18:47 +0900

    initial commit

---
+++ src/main/java/com/ddoong2/requiresnew/RequiresNewApplication.java
@@ -0,0 +1,13 @@
+package com.ddoong2.requiresnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequiresNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequiresNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddoong2/requiresnew/RequiresNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddoong2.requiresnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequiresNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
